ec11f93c6e34c473879cce62a06a44fc22e5191d
Jamison Duarte Da Silva <jamisonduarte@yahoo.com.br>
2017-09-14 21:24:10 -0300

    Projeto Finalizado
    Fica faltando ainda botar a build no dropbox pois ela não cabe no github. Status do projeto 98%

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+    public void MudarDeNivel(string Nivel)
+    {
+        SceneManager.LoadScene(Nivel);
+    }
+}
